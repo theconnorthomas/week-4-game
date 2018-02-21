dabc6c082275812b7201eb628bcb84b08f1335d0
Connor Thomas <connorthomas@connors-mbp.attlocal.net>
2018-02-20 21:15:33 -0600

    added lines to html

---
+++ assets/javascript/game.js
@@ -0,0 +1,48 @@
+/* $(document).ready(function () { */
+
+    var firstGem = Math.floor(Math.random() * 12) + 1;
+    var secondGem = Math.floor(Math.random() * 12) + 1;
+    var thirdGem = Math.floor(Math.random() * 12) + 1;
+    var forthGem = Math.floor(Math.random() * 12) + 1;
+    var targetNumber = Math.floor(Math.random() * 120) + 19;
+    var playerTotal;
+    var
+    console.log(firstGem);
+    console.log(secondGem);
+    console.log(thirdGem);
+    console.log(forthGem);
+    console.log(targetNumber);
+    // Use a function to initialize our calculator.
+    // This way when the user hits clear, we can guarantee a reset of the app.
+
+   /*  function initialize() {
+        firstGem = Math.floor(Math.random() * 12) + 1;
+        secondGem = Math.floor(Math.random() * 12) + 1;
+        thirdGem = Math.floor(Math.random() * 12) + 1;
+        forthGem = Math.floor(Math.random() * 12) + 1;
+        targetNumber = Math.floor(Math.random() * 120) + 19;
+
+
+
+        $("#playerTotal, #second-number, #operator, #result").empty();
+    }
+
+    initialize(); */
+
+    /* // Add an on click listener to all elements that have the class "number"
+    $(".button").on("click", function () {
+
+        // Check if we've already run a calculation, if so... we'll just.
+        if (isCalculated) return;
+
+        // If operator is chosen, we should be writing the secondNumber, otherwise, the firstNumber
+        if (isOperatorChosen) {
+            secondNumber += $(this).val();
+            $("#second-number").text(secondNumber);
+
+        }
+        else {
+            firstNumber += $(this).val();
+            $("#first-number").text(firstNumber);
+        } */
+/* }); */
